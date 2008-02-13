fbabe2ed46298516b450a7b736b08cf496f5dea1
John Wilger <johnwilger@gmail.com>
2008-02-13 18:45:20 +0000

    HTTPFile will raise exceptions if get or put fails.
    git-svn-id: https://code.colortechnology.com/jack_the_ripper/trunk@22 c3fa9876-4ff6-4a47-b9a4-2541730e6fd0

--- lib/jack_the_ripper/http_file.rb
+++ lib/jack_the_ripper/http_file.rb
@@ -21,17 +21,22 @@ module JackTheRIPper
       uri = URI.parse( @uri )
       content_type = MIME::Types.type_for( @path ).first.content_type
       Net::HTTP.start( uri.host, uri.port ) do |http|
-        http.send_request( 'PUT', uri.request_uri, File.read( @path ), { 'Content-Type' => content_type } )
+        result = http.send_request( 'PUT', uri.request_uri, File.read( @path ), { 'Content-Type' => content_type } )
+        result.error! unless result.kind_of?( Net::HTTPSuccess )
       end
     end
     
     class << self
       def get( uri, directory, basename )
         result = Net::HTTP.get_response( URI.parse( uri ) )
-        ext = MIME::Types[ result.content_type ].first.extensions.first
-        file_path = directory + '/' + basename + '.' + ext
-        File.open( file_path, 'w' ) { |f| f.write( result.read_body ) }
-        new( nil, file_path )
+        if result.kind_of?( Net::HTTPSuccess )
+          ext = MIME::Types[ result.content_type ].first.extensions.first
+          file_path = directory + '/' + basename + '.' + ext
+          File.open( file_path, 'w' ) { |f| f.write( result.read_body ) }
+          new( nil, file_path )
+        else
+          result.error!
+        end
       end
     end
   end
--- test/jack_the_ripper/test_http_file.rb
+++ test/jack_the_ripper/test_http_file.rb
@@ -5,8 +5,9 @@ require 'jack_the_ripper/http_file'
 
 class TestJackTheRIPperHTTPFile < Test::Unit::TestCase
   def test_should_get_file_and_store_it_at_specified_path_and_return_http_file_instance
-    http_result = stub( :content_type => 'application/pdf',
-      :read_body => 'file contents' )
+    http_result = Net::HTTPSuccess.allocate
+    http_result.stubs( :content_type ).returns( 'application/pdf' )
+    http_result.stubs( :read_body ).returns( 'file contents' )
     Net::HTTP.expects( :get_response ).
       with( URI.parse( 'http://example.com/file.pdf' ) ).
       returns( http_result )
@@ -40,5 +41,36 @@ class TestJackTheRIPperHTTPFile < Test::Unit::TestCase
       with( 'PUT', uri.request_uri, data, headers ).
       returns( http_response )
     f.put
+  end
+  
+  def test_should_raise_exception_if_get_fails
+    http_result = Net::HTTPServerError.allocate
+    Net::HTTP.expects( :get_response ).
+      with( URI.parse( 'http://example.com/file.pdf' ) ).
+      returns( http_result )
+    http_result.expects( :error! ).raises( Net::HTTPServerException.allocate )
+    assert_raises( Net::HTTPServerException ) do
+      JackTheRIPper::HTTPFile.get( 'http://example.com/file.pdf',
+        '/tmp', 'source' )
+    end
+  end
+  
+  def test_should_raise_exception_if_put_fails
+    f = JackTheRIPper::HTTPFile.new( 'http://example.com/result.jpg',
+      '/tmp/result.jpg' )
+    uri = URI.parse( 'http://example.com/result.jpg' )
+    http_conn = mock
+    Net::HTTP.expects( :start ).
+      with( uri.host, uri.port ).
+      yields( http_conn )
+    headers = { 'Content-Type' => 'image/jpeg' }
+    data = 'file contents'
+    File.expects( :read ).with( '/tmp/result.jpg' ).returns( data )
+    http_response = Net::HTTPServerError.allocate
+    http_conn.expects( :send_request ).
+      with( 'PUT', uri.request_uri, data, headers ).
+      returns( http_response )
+    http_response.expects( :error! ).raises( Net::HTTPServerException.allocate )
+    assert_raises( Net::HTTPServerException ) { f.put }
   end
 end
