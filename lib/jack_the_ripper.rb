f06b606748c09f74a42eb9286ffdb629a8b0e02f
John Wilger <johnwilger@gmail.com>
2008-02-14 18:18:53 +0000

    removed existing release tags, should not have made them yet
    git-svn-id: https://code.colortechnology.com/jack_the_ripper/trunk@31 c3fa9876-4ff6-4a47-b9a4-2541730e6fd0

--- lib/jack_the_ripper.rb
+++ lib/jack_the_ripper.rb
@@ -5,7 +5,7 @@ gem 'right_aws', '= 1.5.0'
 require 'right_aws'
 
 module JackTheRIPper
-  VERSION = '0.1.2'
+  VERSION = '0.1.0'
   
   class RemoteError < StandardError; end
   class ProcessorError < StandardError; end
