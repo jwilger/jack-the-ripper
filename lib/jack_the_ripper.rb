c2223e1f8608c3d433896b2574921af0b4d46b90
John Wilger <johnwilger@gmail.com>
2008-02-19 04:10:28 +0000

    tagging 1.1.0
    git-svn-id: https://code.colortechnology.com/jack_the_ripper/trunk@43 c3fa9876-4ff6-4a47-b9a4-2541730e6fd0

--- lib/jack_the_ripper.rb
+++ lib/jack_the_ripper.rb
@@ -5,7 +5,7 @@ gem 'right_aws', '= 1.5.0'
 require 'right_aws'
 
 module JackTheRIPper
-  VERSION = '1.0.1'
+  VERSION = '1.1.1'
   
   class RemoteError < StandardError; end
   class ProcessorError < StandardError; end
